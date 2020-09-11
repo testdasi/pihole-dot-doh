54193a5c09c069ea92868d2d5738f524eb6520d1
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-11 21:45:53 +0100

    Update install.sh

--- stuff/install.sh
+++ stuff/install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # install stubby config
-cd /temp \
-    && mkdir -p /config/stubby \
-    && cp -n ./stubby.yml /config/stubby/ \
-    && rm -f ./stubby.yml
+#cd /temp \
+#    && mkdir -p /config/stubby \
+#    && cp -n ./stubby.yml /config/stubby/ \
+#    && rm -f ./stubby.yml
 
 # install cloudflared config
-cd /temp \
-    && mkdir -p /config/cloudflared \
-    && cp -n ./cloudflared.yml /config/cloudflared/ \
-    && rm -f ./cloudflared.yml
+#cd /temp \
+#    && mkdir -p /config/cloudflared \
+#    && cp -n ./cloudflared.yml /config/cloudflared/ \
+#    && rm -f ./cloudflared.yml
 
 # run stubby as service
 mkdir -p /etc/services.d/stubby \
     && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/stubby/run \
     && echo 's6-echo "Starting stubby"' >> /etc/services.d/stubby/run \
-    && echo 'stubby -C /config/stubby/stubby.yml' >> /etc/services.d/stubby/run \
+    && echo 'stubby -C /config/stubby.yml' >> /etc/services.d/stubby/run \
     && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/stubby/finish \
     && echo 's6-echo "Stopping stubby"' >> /etc/services.d/stubby/finish \
     && echo 'killall -9 stubby' >> /etc/services.d/stubby/finish
@@ -25,7 +25,7 @@ mkdir -p /etc/services.d/stubby \
 mkdir -p /etc/services.d/cloudflared \
     && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/cloudflared/run \
     && echo 's6-echo "Starting cloudflared"' >> /etc/services.d/cloudflared/run \
-    && echo '/usr/local/bin/cloudflared --config /config/cloudflared/cloudflared.yml' >> /etc/services.d/cloudflared/run \
+    && echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> /etc/services.d/cloudflared/run \
     && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/cloudflared/finish \
     && echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/cloudflared/finish \
     && echo 'killall -9 cloudflared' >> /etc/services.d/cloudflared/finish
