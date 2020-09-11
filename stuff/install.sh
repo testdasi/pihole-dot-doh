60bf9c16f734a73eef6e552e4bfe27a755003b01
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-11 21:34:46 +0100

    Update install.sh

--- stuff/install.sh
+++ stuff/install.sh
@@ -3,43 +3,29 @@
 # install stubby config
 cd /temp \
     && mkdir -p /config/stubby \
-    && cp -n ./stubby.yml /config/stubby/
-#\
-#    && rm -f ./stubby.yml
+    && cp -n ./stubby.yml /config/stubby/ \
+    && rm -f ./stubby.yml
 
 # install cloudflared config
 cd /temp \
     && mkdir -p /config/cloudflared \
-    && cp -n ./cloudflared.yml /config/cloudflared/
-#\
-#    && rm -f ./cloudflared.yml
+    && cp -n ./cloudflared.yml /config/cloudflared/ \
+    && rm -f ./cloudflared.yml
 
-if [ "$DOT" = "yes" ]
-then
-    # run stubby as service
-    mkdir -p /etc/services.d/stubby \
-        && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/stubby/run \
-        && echo 's6-echo "Starting stubby"' >> /etc/services.d/stubby/run \
-        && echo 'stubby -C /config/stubby/stubby.yml' >> /etc/services.d/stubby/run \
-        && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/stubby/finish \
-        && echo 's6-echo "Stopping stubby"' >> /etc/services.d/stubby/finish \
-        && echo 'killall -9 stubby' >> /etc/services.d/stubby/finish
-else
-    rm -f /etc/services.d/stubby/run
-    rm -f /etc/services.d/stubby/finish
-fi
+# run stubby as service
+mkdir -p /etc/services.d/stubby \
+    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/stubby/run \
+    && echo 's6-echo "Starting stubby"' >> /etc/services.d/stubby/run \
+    && echo 'stubby -C /config/stubby/stubby.yml' >> /etc/services.d/stubby/run \
+    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/stubby/finish \
+    && echo 's6-echo "Stopping stubby"' >> /etc/services.d/stubby/finish \
+    && echo 'killall -9 stubby' >> /etc/services.d/stubby/finish
 
-if [ "$DOH" = "yes" ]
-then
-    # run cloudflared as service
-    mkdir -p /etc/services.d/cloudflared \
-        && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/cloudflared/run \
-        && echo 's6-echo "Starting cloudflared"' >> /etc/services.d/cloudflared/run \
-        && echo '/usr/local/bin/cloudflared --config /config/cloudflared/cloudflared.yml' >> /etc/services.d/cloudflared/run \
-        && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/cloudflared/finish \
-        && echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/cloudflared/finish \
-        && echo 'killall -9 cloudflared' >> /etc/services.d/cloudflared/finish
-else
-    rm -f /etc/services.d/cloudflared/run
-    rm -f /etc/services.d/cloudflared/finish
-fi
+# run cloudflared as service
+mkdir -p /etc/services.d/cloudflared \
+    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/cloudflared/run \
+    && echo 's6-echo "Starting cloudflared"' >> /etc/services.d/cloudflared/run \
+    && echo '/usr/local/bin/cloudflared --config /config/cloudflared/cloudflared.yml' >> /etc/services.d/cloudflared/run \
+    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/cloudflared/finish \
+    && echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/cloudflared/finish \
+    && echo 'killall -9 cloudflared' >> /etc/services.d/cloudflared/finish
