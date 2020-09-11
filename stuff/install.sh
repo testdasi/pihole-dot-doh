1b0343ca9b238f54d06c4fe96ec8364ac60413e8
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-11 22:06:36 +0100

    Update install.sh

--- stuff/install.sh
+++ stuff/install.sh
@@ -6,8 +6,8 @@ mkdir -p /etc/services.d/pihole-dot-doh
 # run file
 echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/run
 # Copy config file if not exists
-echo 'cp -n /temp/stubby.yml /config/' > /etc/services.d/pihole-dot-doh/run
-echo 'cp -n /temp/cloudflared.yml /config/' > /etc/services.d/pihole-dot-doh/run
+echo 'cp -n /temp/stubby.yml /config/' >> /etc/services.d/pihole-dot-doh/run
+echo 'cp -n /temp/cloudflared.yml /config/' >> /etc/services.d/pihole-dot-doh/run
 # run stubby as service
 echo 's6-echo "Starting stubby"' >> /etc/services.d/pihole-dot-doh/run
 echo 'stubby -C /config/stubby.yml' >> /etc/services.d/pihole-dot-doh/run
