a83fd34fd797da2a25988f4895ae1c20362b6a06
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-11 22:03:10 +0100

    Update install.sh

--- stuff/install.sh
+++ stuff/install.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
-# install stubby config
-#cd /temp \
-#    && mkdir -p /config/stubby \
-#    && cp -n ./stubby.yml /config/stubby/ \
-#    && rm -f ./stubby.yml
+# Creating pihole-dot-doh service
+mkdir -p /etc/services.d/pihole-dot-doh
 
-# install cloudflared config
-#cd /temp \
-#    && mkdir -p /config/cloudflared \
-#    && cp -n ./cloudflared.yml /config/cloudflared/ \
-#    && rm -f ./cloudflared.yml
-
+# run file
+echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/run
+# Copy config file if not exists
+echo 'cp -n /temp/stubby.yml /config/' > /etc/services.d/pihole-dot-doh/run
+echo 'cp -n /temp/cloudflared.yml /config/' > /etc/services.d/pihole-dot-doh/run
 # run stubby as service
-mkdir -p /etc/services.d/stubby \
-    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/stubby/run \
-    && echo 's6-echo "Starting stubby"' >> /etc/services.d/stubby/run \
-    && echo 'stubby -C /config/stubby.yml' >> /etc/services.d/stubby/run \
-    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/stubby/finish \
-    && echo 's6-echo "Stopping stubby"' >> /etc/services.d/stubby/finish \
-    && echo 'killall -9 stubby' >> /etc/services.d/stubby/finish
-
+echo 's6-echo "Starting stubby"' >> /etc/services.d/pihole-dot-doh/run
+echo 'stubby -C /config/stubby.yml' >> /etc/services.d/pihole-dot-doh/run
 # run cloudflared as service
-mkdir -p /etc/services.d/cloudflared \
-    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/cloudflared/run \
-    && echo 's6-echo "Starting cloudflared"' >> /etc/services.d/cloudflared/run \
-    && echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> /etc/services.d/cloudflared/run \
-    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/cloudflared/finish \
-    && echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/cloudflared/finish \
-    && echo 'killall -9 cloudflared' >> /etc/services.d/cloudflared/finish
+echo 's6-echo "Starting cloudflared"' >> /etc/services.d/pihole-dot-doh/run
+echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> /etc/services.d/pihole-dot-doh/run
+
+# finish file
+echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/finish
+echo 's6-echo "Stopping stubby"' >> /etc/services.d/pihole-dot-doh/finish
+echo 'killall -9 stubby' >> /etc/services.d/pihole-dot-doh/finish
+echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/pihole-dot-doh/finish
+echo 'killall -9 cloudflared' >> /etc/services.d/pihole-dot-doh/finish
