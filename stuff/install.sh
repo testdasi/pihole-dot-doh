92c975210b7c70aa66670dad915e304f0ed9a0bf
testdasi <51292604+testdasi@users.noreply.github.com>
2022-12-25 22:08:03 +1100

    Update install.sh

--- stuff/install.sh
+++ stuff/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 ## Piggy-backing on Pihole service ##
+#(/i insert above, /a insert below)
 # Insert run lines below the call capsh comment
-sed -i "/^# Call capsh with the detected capabilities/a start-stop-daemon --start --background --name cloudflared --chdir \/config --exec \/usr\/local\/bin\/cloudflared -- --config \/config\/cloudflared.yml" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
-sed -i "/^# Call capsh with the detected capabilities/a stubby -g -C \/config\/stubby.yml" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
-sed -i "/^# Call capsh with the detected capabilities/a cp -n \/temp\/stubby.yml \/config/" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
-sed -i "/^# Call capsh with the detected capabilities/a cp -n \/temp\/cloudflared.yml \/config/" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
-# Insert finish lines above kill pihole
-sed -i "/^killall -15 pihole-FTL/i killall -15 cloudflared" /etc/s6-overlay/s6-rc.d/pihole-FTL/finish
-sed -i "/^killall -15 pihole-FTL/i killall -15 stubby" /etc/s6-overlay/s6-rc.d/pihole-FTL/finish
+sed -i "/^capsh /i cp -n \/temp\/stubby.yml \/config/" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
+sed -i "/^capsh /i cp -n \/temp\/cloudflared.yml \/config/" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
+sed -i "/^capsh /i stubby -g -C \/config\/stubby.yml" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
+sed -i "/^capsh /i start-stop-daemon --start --background --name cloudflared --chdir \/config --exec \/usr\/local\/bin\/cloudflared -- --config \/config\/cloudflared.yml" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
+
+# Insert finish lines above kill pihole - temporary disable due to some strange issues
+#sed -i "/^killall -15 pihole-FTL/i killall -15 cloudflared" /etc/s6-overlay/s6-rc.d/pihole-FTL/finish
+#sed -i "/^killall -15 pihole-FTL/i killall -15 stubby" /etc/s6-overlay/s6-rc.d/pihole-FTL/finish
