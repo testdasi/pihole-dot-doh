5e36b88b80fc50d47544f54344b5313c29ffc54e
testdasi <51292604+testdasi@users.noreply.github.com>
2022-12-25 22:27:27 +1100

    Update install.sh

--- stuff/install.sh
+++ stuff/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-## Piggy-backing on Pihole service ##
-#(/i insert above, /a insert below)
-# Insert run lines below the call capsh comment
-sed -i "/^capsh /i cp -n \/temp\/stubby.yml \/config/" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
-sed -i "/^capsh /i cp -n \/temp\/cloudflared.yml \/config/" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
-sed -i "/^capsh /i stubby -g -C \/config\/stubby.yml" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
-sed -i "/^capsh /i start-stop-daemon --start --background --name cloudflared --chdir \/config --exec \/usr\/local\/bin\/cloudflared -- --config \/config\/cloudflared.yml" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
+## Piggy-backing on lighttpd service ##
+# (/i insert above, /a insert below) #
+# Avoid pihole service due to the need to restart for every config change #
+# Insert run lines above the call lighttpd comment
+sed -i "/^lighttpd /i cp -n \/temp\/stubby.yml \/config/" /etc/s6-overlay/s6-rc.d/lighttpd/run
+sed -i "/^lighttpd /i cp -n \/temp\/cloudflared.yml \/config/" /etc/s6-overlay/s6-rc.d/lighttpd/run
+sed -i "/^lighttpd /i stubby -g -C \/config\/stubby.yml" /etc/s6-overlay/s6-rc.d/lighttpd/run
+sed -i "/^lighttpd /i start-stop-daemon --start --background --name cloudflared --chdir \/config --exec \/usr\/local\/bin\/cloudflared -- --config \/config\/cloudflared.yml" /etc/s6-overlay/s6-rc.d/lighttpd/run
 
-# Insert finish lines above kill pihole - temporary disable due to some strange issues
-#sed -i "/^killall -15 pihole-FTL/i killall -15 cloudflared" /etc/s6-overlay/s6-rc.d/pihole-FTL/finish
-#sed -i "/^killall -15 pihole-FTL/i killall -15 stubby" /etc/s6-overlay/s6-rc.d/pihole-FTL/finish
+# Insert finish lines above kill
+sed -i "/^killall -9 lighttpd/i killall cloudflared" /etc/s6-overlay/s6-rc.d/lighttpd/finish
+sed -i "/^killall -9 lighttpd/i killall stubby" /etc/s6-overlay/s6-rc.d/lighttpd/finish
